92e41463cc1cf003040d20dd115c7e087652d048
Arghya Banerjee <arghya.banerjee.dev@gmail.com>
2025-01-07 00:37:09 +0530

    Initial Commit

---
+++ PantheraSphere/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PantheraSphere.Controllers
+{
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        [Route("~/")]
+        [Route("Home")]
+        public IActionResult Home()
+        {
+            return View();
+        }
+    }
+}
---
+++ PantheraSphere/Utility_Classes/Constant.cs
@@ -0,0 +1,16 @@
+﻿public static class Constant
+{
+    public static string usp_Login = "usp_Login";
+    public static string usp_Register = "usp_Register";
+    public static string usp_Menu = "usp_Menu";
+    public static string usp_StockManager = "usp_StockManager";
+    public static string usp_OrderDetails = "usp_OrderDetails";
+    public static string usp_OrderHeader = "usp_OrderHeader";
+    public static string usp_CurrentOrders = "usp_CurrentOrders";
+    public static string usp_BillItems = "usp_BillItems";
+    public static string usp_Invoice = "usp_Invoice";
+    public static string usp_Rating = "usp_Rating";
+    public static string usp_User = "usp_User";
+    public static string usp_OrderHistory = "usp_OrderHistory";
+}
+
