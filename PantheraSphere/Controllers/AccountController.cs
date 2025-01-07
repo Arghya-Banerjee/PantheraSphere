a31f43adfcda76b70a4e5f83effddb00a5624c3b
Arghya Banerjee <arghya.banerjee.dev@gmail.com>
2025-01-07 17:10:58 +0530

    Login Added with Validation

---
+++ PantheraSphere/Controllers/AccountController.cs
@@ -0,0 +1,42 @@
+﻿using Core;
+using Microsoft.AspNetCore.Mvc;
+using PantheraSphere.Models;
+
+namespace PantheraSphere.Controllers
+{
+    [Route("[controller]")]
+    public class AccountController : Controller
+    {
+        [Route("Login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [Route("Register")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost("LoginValidate")]
+        public IActionResult LoginValidate(string username, string password)
+        {
+            LoginModel loginDetails = DBOperations<LoginModel>.GetSpecific(new LoginModel
+            {
+                OpMode = 0, // Validate User
+                UserName = username,
+                PassCode = password
+            }, Constant.usp_Login);
+
+            System.Diagnostics.Debug.WriteLine(username + " Hola " + password);
+
+            if (loginDetails.UserName == null)
+            {
+                return Json(new { success = false, message = "User doesn't exist" });
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ PantheraSphere/Models/LoginModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PantheraSphere.Models
+{
+    public class LoginModel
+    {
+        public int OpMode { get; set; }
+        public string UserName { get; set; }
+        public string PassCode { get; set; }
+        public int RoleID { get; set; }
+    }
+}
